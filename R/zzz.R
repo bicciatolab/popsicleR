23040ae5de30a5aa24e70b68b287f6cd92fd308f
Bicciato Lab <77393945+bicciatolab@users.noreply.github.com>
2021-11-26 10:23:57 +0100

    Uploaded PoPsicleR version 0.1.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    paste0(cat("Hello and welcome to PoPsicle, an interactive pipeline for the preprocessing of single cell data.\nIn this workflow, messages are colour coded: \n"),
+    paste0(cat("Hello and welcome to PoPsicleR, an interactive pipeline for the preprocessing of single cell data.\nIn this workflow, messages are colour coded: \n"),
            cat(green("green messages will provide information on the ongoing step \n")),
            cat(cyan("cyan messages will require the user to provide an input for advancing the script \n")),
            cat(red("red messages will report missing information or wrong inputs \n")),
